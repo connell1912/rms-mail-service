c16ba6bb624a20866c3c3146065b335a26df569a
maresh315 <mareshescoffery@gmail.com>
2020-02-26 16:44:09 -0500

    made small adjustments towards full implementation; needs controller to be finished; need environment variables to be set for smtp username and password

--- mail/src/main/java/com/rms/controller/MailController.java
+++ mail/src/main/java/com/rms/controller/MailController.java
@@ -2,6 +2,8 @@ package com.rms.controller;
 
 import org.springframework.stereotype.Controller;
 
+import com.rms.service.MailService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -15,8 +17,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @CrossOrigin(origins =  "*")
 public class MailController {
-	// @Autowired
-    // MailService ms;
+	@Autowired
+    MailService ms;
     
 	// @Autowired
     // UsersService us;
