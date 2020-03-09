35d9a7b2873e6798e1be1402d7df2617991c2f9a
maresh315 <mareshescoffery@gmail.com>
2020-03-09 16:47:25 -0400

    fixed code smells

--- mail/src/main/java/com/rms/model/Mail.java
+++ mail/src/main/java/com/rms/model/Mail.java
@@ -7,38 +7,33 @@ public class Mail {
 
 	// Replace sender@example.com with your "From" address.
 	// This address must be verified.
-	final private String FROM = "project3.1912@gmail.com";
-	final private String FROMNAME = "Revature RMS";
+	private static final  String FROM = "project3.1912@gmail.com";
+	private static final String FROMNAME = "Revature RMS";
 
 	// Replace recipient@example.com with a "To" address. If your account
 	// is still in the sandbox, this address must be verified.
 	private String to;
 
 	// Replace smtp_username with your Amazon SES SMTP user name.
-	final private String SMTP_USERNAME = System.getenv("SMTP_USERNAME");
+	private static final String SMTP_USERNAME = System.getenv("SMTP_USERNAME");
 
 	// Replace smtp_password with your Amazon SES SMTP password.
-	private final String SMTP_PASSWORD = System.getenv("SMTP_PASSWORD");
-
-	// The name of the Configuration Set to use for this message.
-	// If you comment out or remove this variable, you will also need to
-	// comment out or remove the header below.
-//	    static final String CONFIGSET = "ConfigSet";
+	private static final String SMTP_PASSWORD = System.getenv("SMTP_PASSWORD");
 
 	// Amazon SES SMTP host name. This example uses the US West (Oregon) region.
 	// See
 	// https://docs.aws.amazon.com/ses/latest/DeveloperGuide/regions.html#region-endpoints
 	// for more information.
-	private final String HOST = "email-smtp.us-east-1.amazonaws.com";
+	private static final String HOST = "email-smtp.us-east-1.amazonaws.com";
 
 	// The port you will connect to on the Amazon SES SMTP endpoint.
-	private final int PORT = 587;
+	private static final int PORT = 587;
 
 	// The subject line for the email.
-	private final String SUBJECT = "Password Reset";
+	private static final String SUBJECT = "Password Reset";
 	
 	// The HTML body for the email.
-	private final String BODY = String.join(System.getProperty("line.separator"), "<h1>Password Reset</h1>",
+	private static final String BODY = String.join(System.getProperty("line.separator"), "<h1>Password Reset</h1>",
 			"<p>Reset your password at ",
 			"<a href='http://localhost:3000/passchange'>RMS Reset Password</a>");
 			
@@ -67,10 +62,10 @@ public class Mail {
 	public String getFROMNAME() {
 		return FROMNAME;
 	}
-	public String getSMTP_USERNAME() {
+	public String getSMTPUSERNAME() {
 		return SMTP_USERNAME;
 	}
-	public String getSMTP_PASSWORD() {
+	public String getSMTPPASSWORD() {
 		return SMTP_PASSWORD;
 	}
 	public String getHOST() {
