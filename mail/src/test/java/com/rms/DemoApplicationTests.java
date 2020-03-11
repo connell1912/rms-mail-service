0d18bf85f9e8390ad61b4b9c8777e568f3b51904
maresh315 <mareshescoffery@gmail.com>
2020-03-11 11:10:11 -0400

    -

--- mail/src/test/java/com/rms/DemoApplicationTests.java
+++ mail/src/test/java/com/rms/DemoApplicationTests.java
@@ -1,16 +1,35 @@
 package com.rms;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import com.rms.model.Mail;
+
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	Mail m;
+	@Before
+	void init(){
+		m = new Mail();
+	}
+
 	@Test
 	void contextLoads() {
-        assertTrue(true);
+		assertNotNull(new Mail("mareshescoffery@gmail.com"));
+		assertNotNull(m.getBODY());
+		assertNotNull(m.getFROM();
+		assertNotNull(m.getFROMNAME());
+		assertNotNull(m.getHOST());
+		assertNotNull(m.getSMTPPASSWORD());
+		assertNotNull(m.getSMTPUSERNAME());
+		assertNotNull(m.getSUBJECT());
+		assertTrue(587 == m.getPORT());
+
 	}
 
 }
